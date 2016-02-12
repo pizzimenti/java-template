b625a649d5c361a4b7e8616bf4741781cdaa6e42
Brad Pizzimenti <brad@gennakersystems.com>
2016-02-12 11:32:57 -0800

    template commit of java project and test files

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/landingPage.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/result", (request, response) -> {
+      String textInput = request.queryParams("textInput");
+
+      //call business logic functions here
+
+      HashMap model = new HashMap();
+      model.put("template", "templates/resultPage.vtl");
+      model.put("result", String.format(result));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+      //additional pages would go here
+  }
+
+  //public static 'Returntype' 'FuncName' (Paramtype param) {}  //first business logic function
+
+}
